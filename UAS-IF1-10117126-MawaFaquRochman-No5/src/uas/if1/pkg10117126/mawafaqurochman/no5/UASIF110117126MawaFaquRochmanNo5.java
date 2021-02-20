c7c360d85c494518fe47fa2e382e98e1e57b86d9
mawafaqu <63239367+mawafaqu@users.noreply.github.com>
2021-02-20 13:21:25 +0700

    Add files via upload

---
+++ UAS-IF1-10117126-MawaFaquRochman-No5/src/uas/if1/pkg10117126/mawafaqurochman/no5/UASIF110117126MawaFaquRochmanNo5.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uas.if1.pkg10117126.mawafaqurochman.no5;
+
+/**
+ *
+ * Nama: Mawa Faqu Rochman
+ * Nim : 10117126
+ * Kelas : IF1
+ */
+public class UASIF110117126MawaFaquRochmanNo5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        User m = new User();
+
+        m.setNim("10110269");
+        m.setName("Rizki Adam Kurniawan");
+
+        System.out.println("Nim Mahasiswa: " + m.getNim());
+        System.out.println("Nama Mahasiswa: " + m.getName());
+    }
+
+}
---
+++ UAS-IF1-10117126-MawaFaquRochman-No5/src/uas/if1/pkg10117126/mawafaqurochman/no5/User.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uas.if1.pkg10117126.mawafaqurochman.no5;
+
+/**
+ *
+ * @author MAWA
+ */
+public class User {
+
+    private String nim;
+    private String nama;
+
+    // ini method setter
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    public void setName(String nama) {
+        this.nama = nama;
+    }
+
+    // ini method getter
+    public String getNim() {
+        return this.nim;
+    }
+
+    public String getName() {
+        return this.nama;
+    }
+}
